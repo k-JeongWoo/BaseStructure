784f32d3878fcaeff33988077b64bf5973209922
leecheol-gyu <cheol@neodreams.co.kr>
2021-06-02 21:09:14 +0900

    add Agreement vue

---
+++ src/components/Agreement.vue
@@ -0,0 +1,124 @@
+<template>	<!--  container  -->
+
+  <!--  container  -->
+  <div class="container agreement01">
+    <!-- header -->
+    <header class="header noBg">
+      <div class="headerBox">
+        <a href="#" class="btn_right">
+          <i class="ico_close"></i>
+        </a>
+      </div>
+    </header>
+    <!-- //header -->
+    <!--contents-->
+    <div class="contents">
+      <div class="agreement_box">
+        <h2 class="title_purple tac">
+          서비스 이용약관 동의
+        </h2>
+        <div class="cntAgree">
+          <p class="inputCheck typeA chckAll">
+            <input type="checkbox" name="chckAll" id="chckAll01">
+            <label for="chckAll01">
+              <span class="bul"></span>전체 항목에 동의합니다.
+            </label>
+          </p>
+          <div class="agree_list">
+            <p class="inputCheck typeA">
+              <input type="checkbox" name="chckAgree" id="chckAgree01">
+              <label for="chckAgree01">
+                <span class="bul"></span>통합서비스 이용약관 동의 (필수)
+              </label>
+              <a href="" class="btn_text_size03 txt_r">전체보기</a>
+            </p>
+            <p class="inputCheck typeA">
+              <input type="checkbox" name="chckAgree" id="chckAgree02">
+              <label for="chckAgree02">
+                <span class="bul"></span>민감정보 수진 및 이용 동의 (필수)
+              </label>
+              <a href="" class="btn_text_size03 txt_r">전체보기</a>
+            </p>
+            <p class="inputCheck typeA">
+              <input type="checkbox" name="chckAgree" id="chckAgree03">
+              <label for="chckAgree03">
+                <span class="bul"></span>개인정보 수집 이용 동의  (필수)
+              </label>
+              <a href="" class="btn_text_size03 txt_r">전체보기</a>
+            </p>
+            <p class="inputCheck typeA">
+              <input type="checkbox" name="chckAgree" id="chckAgree04">
+              <label for="chckAgree04">
+                <span class="bul"></span>마케팅정보 수신 동의 (선택)
+              </label>
+              <a href="" class="btn_text_size03 txt_r">전체보기</a>
+            </p>
+            <p class="inputCheck typeA">
+              <label for="chckAgree04">
+                <span class="bul"></span>이름 :
+                <input type="text" v-model="usr_name" />
+              </label>
+            </p>
+            <p class="inputCheck typeA">
+              <label for="chckAgree04">
+                <span class="bul"></span>연락처 :
+                <input type="text" v-model="usr_telnum" />
+              </label>
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <!--//contents-->
+    <!-- footer -->
+    <footer class="footer typeB">
+      <div class="btnArea">
+        <a href="javascript:void(0);" v-on:click="setUsrInfo(usr_name, usr_telnum)" class="btn_fill ">완료</a>
+      </div>
+    </footer>
+    <!-- //footer -->
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      usr_name: '',
+      usr_telnum: ''
+    }
+  },
+  methods: {
+    setUsrInfo: function (usrName, usrTelnum) {
+      axios.get(`/api/user/socialInfo`, {
+      }, {withCredentials: true}
+      ).then(function (response) {
+        console.log(response)
+      })
+    }
+  },
+  created () {
+    axios.get(`/api/user/socialInfo`,
+      {},
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json; charset = utf-8',
+          'Access-Control-Allow-Credentials': 'true'
+        }},
+      {withCredentials: true})
+      .then(function (response) {
+        console.log(response)
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+@import '../assets/resources/css/common.css';
+@import '../assets/resources/css/contents.css';
+</style>
