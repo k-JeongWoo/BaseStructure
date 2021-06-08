b4dcbe446eae9b2c3d3d3ba201fe93baf8b3d66d
K-JeongWoo <moke456@naver.com>
2021-06-08 09:58:08 +0900

    진료프로그램 추가

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <!-- footer -->
+  <div class="footer">
+    <ul>
+      <li class="on">
+        <a href="#">
+          <i class="ico_home"></i>
+          <span>홈</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="ico_data"></i>
+          <span>검진데이터</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="ico_doc"></i>
+          <span>나의 주치의</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="ico_my"></i>
+          <span>마이페이지</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <!-- //footer -->
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
